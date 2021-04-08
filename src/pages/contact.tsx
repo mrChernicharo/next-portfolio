04cac0e5bea32f3e83c7a24395a5d2f21f5d50a2
Felipe Chernicharo <string7dev@gmail.com>
2021-04-08 02:00:16 -0300

    input e textarea

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -45,17 +45,21 @@ function ContactForm() {
         method="POST"
         action="https://formspree.io/f/mbjqjgkw"
       >
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
 
-        <label htmlFor="message">Your Message</label>
-        <textarea id="message" name="message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
+        <div>
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" name="message" />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
 
         <button type="submit" disabled={state.submitting}>
           Submit
